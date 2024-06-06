c87cc5ebf22d92aecd189a16689944bd5aeb14f7
Nate Conley <nate.conley@get10up.com>
2024-06-06 11:33:16 -1000

    Edits JavaScript files for formatting and linting

--- js/hidecss.js
+++ js/hidecss.js
@@ -1,18 +1,14 @@
 /* show/hide on checkbox click */
-    function showMe (box) {
-        
-        var chboxs = document.getElementsByName("mc_nuke_all_styles");
-        var vis = "none";
-        for(var i=0;i<chboxs.length;i++) { 
-            if(chboxs[i].checked){
-             vis = "none";
-            }
-            else{
-              var vis = "";
-              vis = "";
-            }
-        }
-        document.getElementById(box).style.display = vis;
-    
-    
-    }
+// eslint-disable-next-line no-unused-vars -- used on window on click
+function showMe(box) {
+	const chboxs = document.getElementsByName('mc_nuke_all_styles');
+	let vis = 'none';
+	for (let i = 0; i < chboxs.length; i++) {
+		if (chboxs[i].checked) {
+			vis = 'none';
+		} else {
+			vis = '';
+		}
+	}
+	document.getElementById(box).style.display = vis;
+}
