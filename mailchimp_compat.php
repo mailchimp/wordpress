e14b4c03fb4d6d3e5dc8988915166c1af7731339
Nate Conley <nate.conley@get10up.com>
2024-06-06 10:55:07 -1000

    Deprecates signup form improper case function

--- mailchimp_compat.php
+++ mailchimp_compat.php
@@ -119,3 +119,20 @@ if ( ! function_exists( 'is_multisite' ) ) {
 		return false;
 	}
 }
+
+/**
+ * Deprecates functions for plugin version 1.6.0
+ *
+ * Changes function names to snake case
+ */
+
+/**
+ * (Deprecated) Displays a Mailchimp Signup Form
+ *
+ * @param array $args Args
+ * @return void
+ */
+function mailchimpSF_signup_form( $args = array() ) { // phpcs:ignore WordPress.NamingConventions.ValidFunctionName.FunctionNameInvalid -- ignored due to deprecating function
+	_deprecated_function( __FUNCTION__, '1.6.0', 'mailchimp_sf_signup_form' );
+	mailchimp_sf_signup_form( $args );
+}
