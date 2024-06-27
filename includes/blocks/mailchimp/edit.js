73dc4360eeb4ad76d4b711ef7f1b75b07da26fc4
Nate Conley <nate.conley@get10up.com>
2024-06-26 17:58:27 -1000

    Do not SSR in legacy themes

--- includes/blocks/mailchimp/edit.js
+++ includes/blocks/mailchimp/edit.js
@@ -2,14 +2,33 @@ import { useBlockProps } from '@wordpress/block-editor';
 import { __ } from '@wordpress/i18n';
 import { Placeholder, Button, Disabled } from '@wordpress/components';
 import ServerSideRender from '@wordpress/server-side-render';
+import { useSelect } from '@wordpress/data';
 import Icon from './icon';
+
+const disallowedThemesSSR = [
+	'twentytwentyone',
+	'twentytwenty',
+	'twentynineteen',
+	'twentyeighteen',
+	'twentyseventeen',
+	'twentysixteen',
+	'twentyfifteen',
+	'twentyfourteen',
+];
 
 export const BlockEdit = ({ isSelected }) => {
 	const blockProps = useBlockProps();
+	const isDisallowedThemeSSR = useSelect((select) => {
+		const currentTheme = select('core').getCurrentTheme();
+		if (!currentTheme || (!'template') in currentTheme) {
+			return false;
+		}
+		return disallowedThemesSSR.includes(currentTheme.template);
+	});
 
 	return (
 		<div {...blockProps}>
-			{isSelected ? (
+			{isSelected || isDisallowedThemeSSR ? (
 				<Placeholder
 					icon={Icon}
 					label={__('Mailchimp Block', 'mailchimp_i18n')}
