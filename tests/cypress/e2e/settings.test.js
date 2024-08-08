ee0884eb9695b8880e159eab2f4f63daf8ea0fe9
Dharmesh Patel <dspatel44@gmail.com>
2024-08-08 18:06:56 +0530

    Added tests for remove CSS and custom styling.

--- tests/cypress/e2e/settings.test.js
+++ tests/cypress/e2e/settings.test.js
@@ -80,4 +80,59 @@ describe('Admin can update plugin settings', () => {
 		cy.get('#mc_subheader').contains(subHeader);
 		cy.get('#mc_signup_submit').contains(button);
 	});
+
+	it('Admin can remove mailchimp CSS', () => {
+		// Remove mailchimp CSS.
+		cy.visit('/wp-admin/admin.php?page=mailchimp_sf_options');
+		cy.get('#mc_nuke_all_styles').check();
+		cy.get('input[value="Update Subscribe Form Settings"]').first().click();
+
+		// Verify
+		cy.visit(`/?p=${shortcodePostId}`);
+		cy.get('#mc_subheader').should('not.have.css', 'margin-bottom', '18px');
+
+		cy.visit(`/?p=${blockPostId}`);
+		cy.get('#mc_subheader').should('not.have.css', 'margin-bottom', '18px');
+
+		// Enable mailchimp CSS.
+		cy.visit('/wp-admin/admin.php?page=mailchimp_sf_options');
+		cy.get('#mc_nuke_all_styles').uncheck();
+		cy.get('input[value="Update Subscribe Form Settings"]').first().click();
+
+		// Verify
+		cy.visit(`/?p=${shortcodePostId}`);
+		cy.get('#mc_subheader').should('have.css', 'margin-bottom', '18px');
+
+		cy.visit(`/?p=${blockPostId}`);
+		cy.get('#mc_subheader').should('have.css', 'margin-bottom', '18px');
+	});
+
+	it('Admin can set custom styling on signup form', () => {
+		// Remove mailchimp CSS.
+		cy.visit('/wp-admin/admin.php?page=mailchimp_sf_options');
+		cy.get('#mc_custom_style').check();
+		cy.get('#mc_form_border_width').clear().type('10');
+		cy.get('#mc_form_border_color').clear().type('000000');
+		cy.get('#mc_form_text_color').clear().type('FF0000');
+		cy.get('#mc_form_background').clear().type('00FF00');
+		cy.get('input[value="Update Subscribe Form Settings"]').first().click();
+
+		// Verify
+		cy.visit(`/?p=${shortcodePostId}`);
+		cy.get('#mc_signup form').should('have.css', 'border-width', '10px');
+		cy.get('#mc_signup form').should('have.css', 'border-color', 'rgb(0, 0, 0)');
+		cy.get('#mc_signup form').should('have.css', 'color', 'rgb(255, 0, 0)');
+		cy.get('#mc_signup form').should('have.css', 'background-color', 'rgb(0, 255, 0)');
+
+		cy.visit(`/?p=${blockPostId}`);
+		cy.get('#mc_signup form').should('have.css', 'border-width', '10px');
+		cy.get('#mc_signup form').should('have.css', 'border-color', 'rgb(0, 0, 0)');
+		cy.get('#mc_signup form').should('have.css', 'color', 'rgb(255, 0, 0)');
+		cy.get('#mc_signup form').should('have.css', 'background-color', 'rgb(0, 255, 0)');
+
+		// Enable mailchimp CSS.
+		cy.visit('/wp-admin/admin.php?page=mailchimp_sf_options');
+		cy.get('#mc_custom_style').uncheck();
+		cy.get('input[value="Update Subscribe Form Settings"]').first().click();
+	});
 });
