a63ad6072a678bbb44994765c7bd2a54b87f7b60
Dharmesh Patel <dspatel44@gmail.com>
2024-08-08 18:51:12 +0530

    Add logout tests.

--- tests/cypress/e2e/settings.test.js
+++ tests/cypress/e2e/settings.test.js
@@ -189,4 +189,13 @@ describe('Admin can update plugin settings', () => {
 			cy.get('#mc_unsub_link').should('not.exist');
 		});
 	});
+
+	it('Admin can logout', () => {
+		cy.visit('/wp-admin/admin.php?page=mailchimp_sf_options');
+		cy.get('#mailchimp_sf_oauth_connect').should('not.exist');
+		cy.get('input[value="Logout"]').click();
+
+		// connect to "Mailchimp" Account button should be visible.
+		cy.get('#mailchimp_sf_oauth_connect').should('exist');
+	});
 });
