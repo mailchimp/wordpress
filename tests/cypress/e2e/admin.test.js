1b0159378b4a163abe74b87df500055560e9b40a
Dharmesh Patel <dspatel44@gmail.com>
2024-08-08 16:53:04 +0530

    Added admin tests.

--- tests/cypress/e2e/admin.test.js
+++ tests/cypress/e2e/admin.test.js
@@ -1,10 +1,22 @@
-describe( 'Admin can login and make sure plugin is activated', () => {
-	before( () => {
+/* eslint-disable no-undef */
+describe('Admin can login and make sure plugin is activated', () => {
+	before(() => {
 		cy.login();
-	} );
+	});
 
-	it( 'Can deactivate and activate plugin?', () => {
-		cy.deactivatePlugin( 'mailchimp' );
-		cy.activatePlugin( 'mailchimp' );
-	} );
-} );
+	it('Can deactivate and activate plugin?', () => {
+		cy.deactivatePlugin('mailchimp');
+		cy.activatePlugin('mailchimp');
+	});
+
+	it('Can see "Mailchimp" menu and Can visit "Mailchimp" settings page.', () => {
+		cy.visit('/wp-admin/');
+
+		// Check Mailchimp menu.
+		cy.get('#adminmenu li#toplevel_page_mailchimp_sf_options').contains('Mailchimp');
+
+		// Check Heading
+		cy.get('#adminmenu li#toplevel_page_mailchimp_sf_options').click();
+		cy.get('#wpbody .mailchimp-header h1').contains('Mailchimp List Subscribe Form');
+	});
+});
