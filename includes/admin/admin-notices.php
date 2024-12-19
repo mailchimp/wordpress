8382b91cc3f859bc8b98c5afb5e069ddb2645a79
Max Garceau <maxwellgarceau@gmail.com>
2024-12-19 15:22:04 -0500

    Move admin-notices to separate file
    Lint project and fix errors

---
+++ includes/admin/admin-notices.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Admin notices.
+ *
+ * @package Mailchimp
+ */
+
+/**
+ * Display success admin notice.
+ *
+ * NOTE: WordPress localization i18n functionality should be done
+ * on string literals outside of this function in order to work
+ * correctly.
+ *
+ * For more info read here: https://salferrarello.com/why-__-needs-a-hardcoded-string-in-wordpress/
+ *
+ * @since 1.7.0
+ * @param string $msg The message to display.
+ * @return void
+ */
+function mailchimp_sf_admin_notice_success( string $msg ): void {
+	?>
+	<div class="notice notice-success is-dismissible">
+		<p><?php echo esc_html( $msg ); ?></p>
+	</div>
+	<?php
+}
+
+/**
+ * Display error admin notice.
+ *
+ * NOTE: WordPress localization i18n functionality should be done
+ * on string literals outside of this function in order to work
+ * correctly.
+ *
+ * For more info read here: https://salferrarello.com/why-__-needs-a-hardcoded-string-in-wordpress/
+ *
+ * @since 1.7.0
+ * @param string $msg The message to display.
+ * @return void
+ */
+function mailchimp_sf_admin_notice_error( string $msg ): void {
+	?>
+	<div class="notice notice-error">
+		<p><?php echo esc_html( $msg ); ?></p>
+	</div>
+	<?php
+}
