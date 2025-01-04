232852f53474dd304b892e3de3bbb48cb17b2093
MaxwellGarceau <maxwellgarceau@gmail.com>
2025-01-03 19:14:18 -0500

    Add back translators. Fix linting.

--- mailchimp.php
+++ mailchimp.php
@@ -1059,7 +1059,7 @@ function mailchimp_sf_merge_validate_phone( $opt_val, $data ) {
 
 	// Trim whitespace
 	$opt_val = array_map( 'trim', $opt_val ); // Beginning and end
-	$opt_val = array_map( fn($s) => preg_replace( '/\s/', '', $s ), $opt_val ); // Middle
+	$opt_val = array_map( fn( $s ) => preg_replace( '/\s/', '', $s ), $opt_val ); // Middle
 
 	// Format number for validation
 	$opt_val = implode( '-', $opt_val );
@@ -1070,6 +1070,7 @@ function mailchimp_sf_merge_validate_phone( $opt_val, $data ) {
 		 */
 		case strlen( $opt_val ) < 12:
 			$message = sprintf(
+				/* translators: %s: field name */
 				esc_html__( '%s must contain the correct amount of digits', 'mailchimp' ),
 				esc_html( $data['name'] )
 			);
@@ -1081,6 +1082,7 @@ function mailchimp_sf_merge_validate_phone( $opt_val, $data ) {
 		 */
 		case ! preg_match( '/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/', $opt_val ):
 			$message = sprintf(
+				/* translators: %s: field name */
 				esc_html__( '%s must consist of only numbers', 'mailchimp' ),
 				esc_html( $data['name'] )
 			);
