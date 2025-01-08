bc69c44b902045ee5e6b26e3124007d22bac41c5
MaxwellGarceau <maxwellgarceau@gmail.com>
2025-01-08 17:01:57 -0500

    Update tests to ensure empty and falsy inputs return null
    Expand validation test cases

--- tests/unit/USPhoneNumberValidationTest.php
+++ tests/unit/USPhoneNumberValidationTest.php
@@ -57,13 +57,15 @@ public static function invalidPhoneNumbersProvider(): array {
 		// Mixed alphabets and numbers
 		[['12a', '456', '7b90'], ['name' => 'Phone']],
 		// Symbols like hyphens, slashes, or parentheses
-		[['123-', '456', '7890'], ['name' => 'Phone']],
-		[['123', '/456', '7890'], ['name' => 'Phone']],
+		[['12-', '456', '7890'], ['name' => 'Phone']],
+		[['123', '/45', '7890'], ['name' => 'Phone']],
 		[['(12', '456', '7890'], ['name' => 'Phone']],
-		// Decimal points
-		[['1.4', '567', '8901'], ['name' => 'Phone']],
-		// Emoji or Unicode characters
-		[['123', '📞46', '7890'], ['name' => 'Phone']],
+
+		[['1.4', '567', '8901'], ['name' => 'Phone']], // Decimal points
+		[['---', '---', '----'], ['name' => 'Phone']], // All hyphen
+		[['12_', '_45', '78__'], ['name' => 'Phone']], // Underscores
+		[['123', "---", '7890'], ['name' => 'Phone']], // Mix with placeholder dashes
+		[['123', "(45", '7890'], ['name' => 'Phone']], // Mix with partial input artifact
 	];
 }
 
@@ -74,7 +76,8 @@ public static function invalidPhoneNumbersProvider(): array {
 	 */
 	public static function tooShortPhoneNumbersProvider(): array {
 		return [
-			// [['', '', ''], ['name' => 'Phone']], // All empty is not an error, this is a black phone field
+			// Not sure how many characters emojis are, but they shouldn't pass validation
+			[['123', '📞46', '7890'], ['name' => 'Phone']], // Emoji or Unicode characters
 
 			[['12 ', '456', '789'], ['name' => 'Phone']], // 1st box whitespace
 			[['123', '45 ', '7890'], ['name' => 'Phone']], // 2nd box whitespace
@@ -84,6 +87,46 @@ public static function invalidPhoneNumbersProvider(): array {
 			[['123', '456', '890'], ['name' => 'Phone']], // 3rd box short
 			[[null, '456', '7890'], ['name' => 'Phone']], // Null values
 			[['123', "\r0", '7890'], ['name' => 'Phone']], // Control characters (tab)
+			[[' ', '  ', '   '], ['name' => 'Phone']], // All whitespaces
+			[["\t", "\n", "\r"], ['name' => 'Phone']], // All control chars (whitespace)
+			[['(', ') ', '-'], ['name' => 'Phone']], // Input Mask Artifacts
+			[['\'', '\'', '\''], ['name' => 'Phone']], // Single-Quote or Similar Characters
+			[["\u00A0", "\u2007", "\u202F"], ['name' => 'Phone']], // Non-breaking space variants
+			[["\u200B", "\u2060", "\uFEFF"], ['name' => 'Phone']], // Invisible Unicode Characters
+			[["&nbsp;", "&#160;", "&#x20;"], ['name' => 'Phone']], // HTML entities and encodings
+
+			// Falsy and real values mixed
+			[['123', '', '7890'], ['name' => 'Phone']], // Mix with empty string
+			[['123', null, '7890'], ['name' => 'Phone']], // Mix with null
+			[['123', false, '7890'], ['name' => 'Phone']], // Mix with false
+			[['123', [], '7890'], ['name' => 'Phone']], // Mix with array
+			[['123', 0, '7890'], ['name' => 'Phone']], // Mix with zero
+			[['123', "\t", '7890'], ['name' => 'Phone']], // Mix with tab character
+			[['123', "\n", '7890'], ['name' => 'Phone']], // Mix with newline
+			[['123', "\u200B", '7890'], ['name' => 'Phone']], // Mix with zero-width space
+			[['123', "__", '7890'], ['name' => 'Phone']], // Mix with underscore
+			[['123', "&nbsp;", '7890'], ['name' => 'Phone']], // Mix with HTML entity
+			[['123', "&#160;", '7890'], ['name' => 'Phone']], // Mix with numeric HTML entity
+			[['123', "\u00A0", '7890'], ['name' => 'Phone']], // Mix with non-breaking space
+			[['123', "\r", '7890'], ['name' => 'Phone']], // Mix with carriage return
+		];
+	}
+
+	/**
+	 * Data provider for falsy and blank phone numbers.
+	 *
+	 * @return array
+	 */
+	public static function falsyAndBlankPhoneNumbersProvider() {
+		return [
+			[[], ['name' => 'Phone']], // Empty array
+			[['', '', ''], ['name' => 'Phone']], // All empty is not an error, this is a blank phone field
+			[[null, null, null], ['name' => 'Phone']], // All null
+			[[false, false, false], ['name' => 'Phone']], // All false
+			[[[], [], []], ['name' => 'Phone']], // All array
+			[[0, 0, 0], ['name' => 'Phone']], // All zero
+			[[false, '', 0], ['name' => 'Phone']], // Mixed falsy values
+			[[null, '', []], ['name' => 'Phone']], // Mixed falsy values
 		];
 	}
 
@@ -150,7 +193,7 @@ public static function invalidPhoneNumbersProvider(): array {
 	 *
 	 * @dataProvider tooShortPhoneNumbersProvider
 	 */
-	public function testTooShortPhoneNumbers($input, $data): void {
+	public function testTooShortPhoneNumbers($phoneArr, $formData): void {
 		$wp_error = Mockery::mock('WP_Error');
 		$wp_error_factory = function ($code, $message, $data = null) use ($wp_error) {
 			$wp_error
@@ -164,7 +207,7 @@ public static function invalidPhoneNumbersProvider(): array {
 		};
 
 		$validate_merge_fields = new Validate_Merge_Fields($wp_error_factory);
-		$result = $validate_merge_fields->validate_phone($input, $data);
+		$result = $validate_merge_fields->validate_phone($phoneArr, $formData);
 
 		$this->assertInstanceOf(WP_Error::class, $result);
 		$this->assertEquals(Validate_Merge_Fields::PHONE_VALIDATION_ERROR_CODE, $result->get_error_code());
@@ -172,5 +215,20 @@ public static function invalidPhoneNumbersProvider(): array {
 			'/must contain the correct amount of digits/',
 			$result->get_error_message(Validate_Merge_Fields::PHONE_VALIDATION_ERROR_CODE)
 		);
+	}
+
+	/**
+	 * Test falsy and empty inputs return null.
+	 * 
+	 * null must be returned for empty phone numbers in order to
+	 * not throw validation errors
+	 *
+	 * @dataProvider falsyAndBlankPhoneNumbersProvider
+	 */
+	public function testFalsyAndEmptyPhoneNumbersReturnNull($phoneArr, $formData) {
+		$validate_merge_fields = new Validate_Merge_Fields();
+		$result = $validate_merge_fields->validate_phone($phoneArr, $formData);
+
+		$this->assertEquals(null, $result);
 	}
 }
