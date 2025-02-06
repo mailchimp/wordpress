dfe5e3ab304f10a6f1865a33a5f50b83b6f5392e
Dharmesh Patel <dspatel44@gmail.com>
2025-02-06 19:01:39 +0530

    Remove "CSS Cheat Sheet" section from settings.

--- views/setup_page.php
+++ views/setup_page.php
@@ -391,65 +391,6 @@ $is_list_selected = false;
 				}
 			}
 			?>
-
-			<div class="mc-section" style="margin-top: 35px;">
-				<table class="widefat mc-widefat">
-					<tr><th colspan="2"><?php esc_html_e( 'CSS Cheat Sheet', 'mailchimp' ); ?></th></tr>
-					<tr valign="top">
-						<th scope="row">.widget_mailchimpsf_widget </th>
-						<td><?php esc_html_e( 'This targets the entire widget container.', 'mailchimp' ); ?></td>
-					</tr>
-					<tr valign="top">
-						<th scope="row">.widget-title</th>
-						<td>
-							<?php
-							echo wp_kses(
-								__( 'This styles the title of your Mailchimp widget. <i>Modifying this class will affect your other widget titles.</i>', 'mailchimp' ),
-								[
-									'i' => [],
-								]
-							);
-							?>
-						</td>
-					</tr>
-					<tr valign="top">
-						<th scope="row">#mc_signup</th>
-						<td><?php esc_html_e( 'This targets the entirity of the widget beneath the widget title.', 'mailchimp' ); ?></td>
-					</tr>
-					<tr valign="top">
-						<th scope="row">#mc_subheader</th>
-						<td><?php esc_html_e( 'This styles the subheader text.', 'mailchimp' ); ?></td>
-					</tr>
-					<tr valign="top">
-						<th scope="row">.mc_form_inside</th>
-						<td><?php esc_html_e( 'The guts and main container for the all of the form elements (the entirety of the widget minus the header and the sub header).', 'mailchimp' ); ?></td>
-					</tr>
-					<tr valign="top">
-						<th scope="row">.mc_header</th>
-						<td><?php esc_html_e( 'This targets the label above the input fields.', 'mailchimp' ); ?></td>
-					</tr>
-					<tr valign="top">
-						<th scope="row">.mc_input</th>
-						<td><?php esc_html_e( 'This attaches to the input fields.', 'mailchimp' ); ?></td>
-					</tr>
-					<tr valign="top">
-						<th scope="row">.mc_header_address</th>
-						<td><?php esc_html_e( 'This is the label above an address group.', 'mailchimp' ); ?></td>
-					</tr>
-					<tr valign="top">
-						<th scope="row">.mc_radio_label</th>
-						<td><?php esc_html_e( 'These are the labels associated with radio buttons.', 'mailchimp' ); ?></td>
-					</tr>
-					<tr valign="top">
-						<th scope="row">#mc-indicates-required</th>
-						<td><?php esc_html_e( 'This targets the “Indicates Required Field” text.', 'mailchimp' ); ?></td>
-					</tr>
-					<tr valign="top">
-						<th scope="row">#mc_signup_submit</th>
-						<td><?php esc_html_e( 'Use this to style the submit button.', 'mailchimp' ); ?></td>
-					</tr>
-				</table>
-			</div>
 		</form>
 	</div>
 </div>
