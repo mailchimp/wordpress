d6255045715e6abf7b416ff943b1f73e9b3fe5a8
Darin Kotter <darin.kotter@gmail.com>
2025-04-10 09:28:25 -0600

    Update minimum version to follow our L-4 standard

--- mailchimp.php
+++ mailchimp.php
@@ -5,7 +5,7 @@
  * Description:       Add a Mailchimp signup form block, widget or shortcode to your WordPress site.
  * Text Domain:       mailchimp
  * Version:           1.7.0
- * Requires at least: 6.6
+ * Requires at least: 6.4
  * Requires PHP:      7.0
  * PHP tested up to:  8.3
  * Author:            Mailchimp
