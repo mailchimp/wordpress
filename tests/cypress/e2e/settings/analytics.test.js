49c418c2d1567f8dd178fa15925099925045a205
alaca <ante.laca@gmail.com>
2026-03-24 11:55:04 +0100

    feature: analytics test

---
+++ tests/cypress/e2e/settings/analytics.test.js
@@ -0,0 +1,97 @@
+/* eslint-disable no-undef */
+describe('Analytics admin page', () => {
+	before(() => {
+		cy.login();
+	});
+
+	describe('When connected', () => {
+		it('Can see "Analytics" submenu under Mailchimp menu', () => {
+			cy.visit('/wp-admin/');
+			cy.get('#adminmenu li#toplevel_page_mailchimp_sf_options').click();
+			cy.get('#adminmenu li#toplevel_page_mailchimp_sf_options .wp-submenu')
+				.contains('Analytics')
+				.should('be.visible');
+		});
+
+		it('Can visit Analytics page and see the heading', () => {
+			cy.visit('/wp-admin/admin.php?page=mailchimp_sf_analytics');
+			cy.get('#wpbody h1.mailchimp-sf-settings-page-hero-title').contains('Analytics');
+		});
+
+		it('Analytics page loads required assets', () => {
+			cy.visit('/wp-admin/admin.php?page=mailchimp_sf_analytics');
+
+			cy.get('script[id="mailchimp_sf_chartjs-js"]').should('exist');
+			cy.get('script[id="mailchimp_sf_analytics_js-js"]').should('exist');
+			cy.get('link[id="mailchimp_sf_analytics_css-css"]').should('exist');
+		});
+
+		it('Assets do not load on other admin pages', () => {
+			cy.visit('/wp-admin/admin.php?page=mailchimp_sf_options');
+
+			cy.get('script[id="mailchimp_sf_chartjs-js"]').should('not.exist');
+			cy.get('script[id="mailchimp_sf_analytics_js-js"]').should('not.exist');
+			cy.get('link[id="mailchimp_sf_analytics_css-css"]').should('not.exist');
+		});
+
+		it('Date range filter defaults to "Last 30 days"', () => {
+			cy.visit('/wp-admin/admin.php?page=mailchimp_sf_analytics');
+			cy.get('#mailchimp-sf-date-range').should('have.value', '30');
+		});
+
+		it('Custom date inputs are hidden by default', () => {
+			cy.visit('/wp-admin/admin.php?page=mailchimp_sf_analytics');
+			cy.get('.mailchimp-sf-custom-dates').should('not.be.visible');
+		});
+
+		it('Selecting "Custom" shows custom date inputs', () => {
+			cy.visit('/wp-admin/admin.php?page=mailchimp_sf_analytics');
+			cy.get('#mailchimp-sf-date-range').select('custom');
+			cy.get('.mailchimp-sf-custom-dates').should('be.visible');
+			cy.get('#mailchimp-sf-date-from').should('be.visible');
+			cy.get('#mailchimp-sf-date-to').should('be.visible');
+		});
+
+		it('Switching back to a preset hides custom date inputs', () => {
+			cy.visit('/wp-admin/admin.php?page=mailchimp_sf_analytics');
+			cy.get('#mailchimp-sf-date-range').select('custom');
+			cy.get('.mailchimp-sf-custom-dates').should('be.visible');
+			cy.get('#mailchimp-sf-date-range').select('7');
+			cy.get('.mailchimp-sf-custom-dates').should('not.be.visible');
+		});
+
+		it('Resolved date range display updates on filter change', () => {
+			cy.visit('/wp-admin/admin.php?page=mailchimp_sf_analytics');
+			cy.get('#mailchimp-sf-resolved-date-range').should('not.be.empty');
+			cy.get('#mailchimp-sf-date-range').select('7');
+			cy.get('#mailchimp-sf-resolved-date-range').should('not.be.empty');
+		});
+
+		it('List filter is present with options', () => {
+			cy.visit('/wp-admin/admin.php?page=mailchimp_sf_analytics');
+			cy.get('#mailchimp-sf-list-filter').should('exist');
+			cy.get('#mailchimp-sf-list-filter option').should('have.length.greaterThan', 0);
+		});
+	});
+
+	describe('When not connected', () => {
+		before(() => {
+			cy.login();
+			cy.visit('/wp-admin/admin.php?page=mailchimp_sf_options');
+			cy.mailchimpLogout();
+		});
+
+		it('Analytics submenu is not visible', () => {
+			cy.visit('/wp-admin/');
+			cy.get('#adminmenu li#toplevel_page_mailchimp_sf_options').click();
+			cy.get('#adminmenu li#toplevel_page_mailchimp_sf_options .wp-submenu')
+				.should('not.contain', 'Analytics');
+		});
+
+		after(() => {
+			cy.login();
+			cy.visit('/wp-admin/admin.php?page=mailchimp_sf_options');
+			cy.mailchimpLoginIfNotAlreadyLoggedIn();
+		});
+	});
+});
